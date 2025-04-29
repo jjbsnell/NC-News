5163287050f6f833b43e8389d35d8ed67775fcfe
jjbsnell <jack.jjs.snell@gmail.com>
2025-04-29 11:51:56 +0100

    task 2

--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
-const express = require("express");
+
 const app = require("./app.js");
-const db = require("./db/connection");
+
 
 app.listen(9090, () => {
   console.log("listening on port 9090");
